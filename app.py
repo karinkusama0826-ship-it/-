1896a902a5ceeaeed61af205bdbd2197404f1003
karinkusama0826-ship-it <karinkusama0826@gmail.com>
2026-02-24 01:02:02 +0900

    Added Dev Container Folder

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import streamlit as st
 
 # --- 設定（ここを好きな言葉に変えてください） ---
-APP_TITLE = "読者限定！プレゼント受け取り会場"
+APP_TITLE = "QRコード読み取ってくれてありがとう！💝"
 THANK_YOU_MESSAGE = "メッセージありがとうございます！お礼にこちらの特典をお受け取りください。"
-SECRET_REPLY = "【運営者からのメッセージ】いつも応援ありがとうございます。これからも頑張ります！"
-# 特典画像（今はサンプルです。あとで自分の画像のURLに変えられます）
-IMAGE_URL = "https://placehold.jp/24/cc9999/ffffff/400x300.png?text=Special%20Gift"
+SECRET_REPLY = "【運営者からのメッセージ】いつも頑張っていますね！毎日お疲れ様です！"
+# 特典画像
+IMAGE_URL =https://imgur.com/a/TNEBX9P
+
 
 # --- アプリの画面構成 ---
 st.set_page_config(page_title=APP_TITLE, page_icon="🎁")
@@ -14,7 +15,7 @@ st.write("QRコードからのアクセスありがとうございます！")
 
 with st.container():
     st.subheader("メッセージを送る")
-    user_msg = st.text_area("感想やメッセージを教えてください👇", placeholder="例：いつも見てます！")
+    user_msg = st.text_area("今日がんばったことを教えてください", placeholder="例：いつも見てます！")
     send_btn = st.button("メッセージを送信して特典を見る")
 
 if send_btn:
